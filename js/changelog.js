5228cf85c5f4e1d42246a6a06950e736521f2630
TheRatCircus <bloxbuilder@gmail.com>
2021-02-04 10:26:26 -0500

    Expand/collapse all buttons for changelog
    Additionally:
    - Normalize quotation symbols.
    - Fix some indenting.
    - Give the changelog a button to go back home.
    - Use jQuery to create changelog header event handlers.
    - Add a transition duration to changelog headers.

--- js/changelog.js
+++ js/changelog.js
@@ -1,43 +1,59 @@
-var file = 'https://raw.githubusercontent.com/LOUD-Project/Git-LOUD/master/CHANGELOG.txt';
-jQuery.get(file, function(data) {
-	data = data.replaceAll(/(['"&])/g, function(match) {
-		if (match == "'") {
-			return "&apos;";
-		} else if (match == '"') {
-			return "&quot;";
-		} else {
-			return "&amp;";
+$(document).ready(function() {
+	var file = 'https://raw.githubusercontent.com/LOUD-Project/Git-LOUD/master/CHANGELOG.txt';
+	jQuery.get(file, function(data) {
+		data = data.replaceAll(/(['"&])/g, function(match) {
+			if (match == "'") {
+				return "&apos;";
+			} else if (match == '"') {
+				return "&quot;";
+			} else {
+				return "&amp;";
+			}
+		});
+
+		var entries = data.match(/###[\w\s\-]+###([\w\s\-.:|>'(),;&\/“”’]|(#(?!#)))+/g);
+		for (const entry of entries) {
+			var lines = entry.split('\n');
+			var header = lines[0].substring(4, lines[0].length - 4);
+			var btn = $(`<button class="changelog-header">${header}</button>`);
+			$("#changelog-container").append(btn);
+			var body = $('<div class="changelog-entry"></div>')
+			$("#changelog-container").append(body);
+			for (var i = 1; i < (lines.length - 2); i++) {
+				if (lines[i]) {
+					body.append(`<p>${lines[i]}</p>`);
+				} else {
+					body.append("<br>")
+				}
+			}
+		}
+
+		$("body").on("click", ".changelog-header", function() {
+			this.classList.toggle("active");
+			var content = this.nextElementSibling;
+			if (content.style.display === "block") {
+				content.style.display = "none";
+			} else {
+				content.style.display = "block";
+			}
+		});
+	});
+
+	$("#changelog-coll-all").click(function() {
+		var coll = $(".changelog-header");
+		var i;
+		for (i = 0; i < coll.length; i++) {
+			coll[i].classList.remove("active");
+			coll[i].nextElementSibling.style.display = "none";
 		}
 	});
 
-	var entries = data.match(/###[\w\s\-]+###([\w\s\-.:|>'(),;&\/“”’]|(#(?!#)))+/g);
-	for (const entry of entries) {
-		var lines = entry.split('\n');
-		var header = lines[0].substring(4, lines[0].length - 4);
-		var btn = $(`<button class="changelog-header">${header}</button>`);
-		$('#changelog-container').append(btn);
-		var body = $('<div class="changelog-entry"></div>')
-		$('#changelog-container').append(body);
-		for (var i = 1; i < (lines.length - 2); i++) {
-			if (lines[i]) {
-				body.append(`<p>${lines[i]}</p>`);
-			} else {
-				body.append('<br>')
-			}
+	$("#changelog-exp-all").click(function() {
+		var coll = $(".changelog-header");
+		var i;
+		for (i = 0; i < coll.length; i++) {
+			coll[i].classList.add("active");
+			coll[i].nextElementSibling.style.display = "block";
 		}
-	}
-
-	var coll = $(".changelog-header");
-	var i;
-	for (i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", function() {
-		  this.classList.toggle("active");
-		  var content = this.nextElementSibling;
-		  if (content.style.display === "block") {
-			content.style.display = "none";
-		  } else {
-			content.style.display = "block";
-		  }
-		});
-	  }
+	});
 });
